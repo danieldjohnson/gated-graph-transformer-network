f9cfd9586efacc96be4f722d7764907548a8fcbf
hexahedria <hexahedria@gmail.com>
2016-07-16 14:00:29 -0700

    Update convert_story utility to work with preprocessing

--- convert_story.py
+++ convert_story.py
@@ -2,6 +2,9 @@ import argparse
 import os
 import numpy as np
 import babi_graph_parse
+from babi_graph_parse import PreppedStory
+import gzip
+import pickle
 
 def convert(story):
     # import pdb; pdb.set_trace()
@@ -29,24 +32,23 @@ def convert(story):
 
     return tuple(x[np.newaxis,...] for x in (all_node_strengths, all_node_ids, all_node_states, all_edges))
 
-def main(task_fn, index1, index2, outputdir, dynamic=True):
-    prepped_stories = babi_graph_parse.prepare_stories(babi_graph_parse.get_stories(task_fn), dynamic)
-    sentence_length, new_nodes_per_iter, buckets, wordlist, anslist, graph_node_list, graph_edge_list, bucketed = prepped_stories
-    print("buckets:", buckets)
-    story = bucketed[index1][index2]
+def main(storyfile, outputdir):
+    
+    with gzip.open(storyfile,'rb') as f:
+        story, sents, query, ans = pickle.load(f)
+
+    with open(os.path.join(outputdir,'story.txt'),'w') as f:
+        f.write("{}\n{}\n{}".format("\n".join(" ".join(s) for s in sents), " ".join(query), " ".join(ans)))
+
     results = convert(story)
     if not os.path.exists(outputdir):
         os.makedirs(outputdir)
     for i,res in enumerate(results):
-        print(res.shape)
         np.save(os.path.join(outputdir,'result_{}.npy'.format(i)), res)
 
-parser = argparse.ArgumentParser(description='Generate an ngrams task')
-parser.add_argument("task_fn", help="Task filename")
-parser.add_argument("index1", type=int, help="Bucket index")
-parser.add_argument("index2", type=int, help="Story index")
+parser = argparse.ArgumentParser(description='Convert a story to graph')
+parser.add_argument("storyfile", help="Story filename")
 parser.add_argument("outputdir", help="Output directory")
-parser.add_argument("--static", action="store_false", dest="dynamic", help="Don't use dynamic mode")
 
 if __name__ == '__main__':
     args = vars(parser.parse_args())
